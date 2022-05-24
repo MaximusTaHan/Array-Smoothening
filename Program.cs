0c41fe132048a6343f937be35a2e55de0efb0ff1
Max <Max.95hansen@gmail.com>
2022-05-24 15:17:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        string n = Console.ReadLine();
+        string inParams = Console.ReadLine();
+
+        int[] conditions = n.Split(" ").Select(int.Parse).ToArray();
+        string[] basicTestCase = inParams.Split(" ").ToArray();
+
+        var distinctNumsCount = new Dictionary<int, int>();
+
+        foreach (string i in basicTestCase)
+        {
+            int num = int.Parse(i);
+            if (distinctNumsCount.ContainsKey(num))
+                distinctNumsCount[num]++;
+            else
+                distinctNumsCount.Add(num, 1);
+        }
+
+        if (conditions[1] == 0)
+        {
+            Console.WriteLine(distinctNumsCount.Values.Max());
+        }
+        else
+        {
+
+            for (int i = 0; i < conditions[1]; i++)
+            {
+                KeyValuePair<int, int> pair = new KeyValuePair<int, int>(0, 0);
+                foreach (var kvp in distinctNumsCount)
+                {
+                    if (kvp.Value > pair.Value)
+                        pair = kvp;
+                }
+
+                distinctNumsCount[pair.Key] -= 1;
+            }
+            Console.WriteLine(distinctNumsCount.Values.Max());
+        }
+    }
+}
